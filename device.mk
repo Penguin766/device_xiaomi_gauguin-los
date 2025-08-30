db72cb06d1af4f391bb99bcaf1471802e5533938
Penguin766 <83493195+Penguin766@users.noreply.github.com>
2025-08-31 01:19:06 +0200

    gauguin: Migrate to AIDL camera provider

--- device.mk
+++ device.mk
@@ -126,8 +126,7 @@ $(call soong_config_set,camera,override_format_from_reserved,true)
 $(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.xiaomi_gauguin)
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider-service.lineage
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
